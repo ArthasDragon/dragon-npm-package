b33af546fec3b21aaeaa6a93942db96a1d7c8c90
an_ying <ywl123698745>
2018-07-20 14:05:21 +0800

    Merge branch 'develop'

--- packages/hlj-antd-cmps/scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# clean
-rm -rf lib
-
-
-# build umd output
-cross-env NODE_ENV=production webpack --progress
-
---
+++ packages/hlj-antd-scripts/bin/antd-scripts.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const { spawnSync } = require('child_process')
+const script = process.argv[2]
+
+switch (script) {
+  case 'start':
+  case 'build':
+    const res = spawnSync('node', [require.resolve('../scripts/' + script)], {
+      stdio: 'inherit'
+    })
+    break
+  default:
+    console.log(`Unknown script ${script}.`)
+    break
+}
