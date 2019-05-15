d62d0f6ee7c74c6e30857efa1576aef6c698acf8
ArthasDragon <1641627793@qq.com>
2019-05-15 12:13:51 +0800

    update

---
+++ packages/hnao-cli/templetes/vue/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  moduleFileExtensions: ['js'],
+  testMatch: ['**/**/__tests__/**/*.test.js?(x)'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  },
+  transformIgnorePatterns: ['/node_modules/', '.history'],
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  }
+}
