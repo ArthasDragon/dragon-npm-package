18435a019292590c4ca40a17f3814b66168bc10a
ArthasDragon <1641627793@qq.com>
2019-12-11 10:24:47 +0800

    update

--- packages/dragon-cli/templates/react/src/App.tsx
+++ packages/dragon-cli/templates/react/src/App.tsx
@@ -3,10 +3,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import routerConfig from './routerConfig';
 import { RouteWithSubRoutes } from '@util/index';
 
-export default () => (
+const App = () => (
   <Router>
     {routerConfig.map((route, i) => (
       <RouteWithSubRoutes key={i} {...route} />
     ))}
   </Router>
 );
+export default App;
--- packages/dragon-cli/templates/react/src/utils/loadCmp.tsx
+++ packages/dragon-cli/templates/react/src/utils/loadCmp.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import * as Loadable from 'react-loadable';
 
+function Loading() {
+  return <div>加载中...</div>;
+}
+
 export const asyncLoad = path =>
   Loadable({
     loader: () => import(`@/pages/${path}`),
-    loading: () => <div>加载中...</div>,
+    loading: Loading,
   });
